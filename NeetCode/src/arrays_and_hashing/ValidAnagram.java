2802ea5d52d564259bf23123404c95ddb9776d2b
Aslam Hossain <aslam.ece.hstu@gmail.com>
2024-09-16 13:48:17 +0600

    Anagram validation is added

---
+++ NeetCode/src/arrays_and_hashing/ValidAnagram.java
@@ -0,0 +1,30 @@
+package arrays_and_hashing;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Sep 16, 2024
+ */
+public class ValidAnagram {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println("Is Anagram: " + isAnagram(s, t));
+    }
+
+    private static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] store = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            store[s.charAt(i) - 'a']++;
+            store[t.charAt(i) - 'a']--;
+        }
+        for (int n : store) {
+            if (n != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

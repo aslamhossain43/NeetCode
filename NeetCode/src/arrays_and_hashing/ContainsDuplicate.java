7ba45e31ca81dc1deb4956bd76ccf14bd23d62a0
Aslam Hossain <aslam.ece.hstu@gmail.com>
2024-09-16 11:37:12 +0600

    1. contains duplicate

--- NeetCode/src/arrays_and_hashing/ContainsDuplicate.java
+++ NeetCode/src/arrays_and_hashing/ContainsDuplicate.java
@@ -1,11 +1,26 @@
 package arrays_and_hashing;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @Author Md. Aslam Hossain
  * @Date Sep 16, 2024
  */
 public class ContainsDuplicate {
     public static void main(String[] args) {
+        int[] nums = {3, 1, 8, 4, 5, 6, 3};
+        System.out.println("Is Duplicate: " + containsDuplicate(nums));
+    }
 
+    private static boolean containsDuplicate(int[] nums) {
+        Set<Integer> uniques = new HashSet<>();
+        for (Integer integer : nums) {
+            if (uniques.contains(integer)) {
+                return true;
+            }
+            uniques.add(integer);
+        }
+        return false;
     }
 }

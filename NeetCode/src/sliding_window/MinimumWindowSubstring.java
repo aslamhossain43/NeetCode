500edcca800492be3d675fbb7216d90a97c9d83c
Aslam Hossain <aslam.ece.hstu@gmail.com>
2024-09-27 09:41:13 +0600

    Minimum window substring is added

---
+++ NeetCode/src/sliding_window/MinimumWindowSubstring.java
@@ -0,0 +1,51 @@
+package sliding_window;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Sep 27, 2024
+ */
+public class MinimumWindowSubstring {
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        System.out.println("Minimum window substring is: " + minWindow(s, t));
+    }
+
+    private static String minWindow(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int matched = 0;
+        int start = 0;
+        int minLen = s.length() + 1;
+        int subStr = 0;
+
+        for (int endWindow = 0; endWindow < s.length(); endWindow++) {
+            char right = s.charAt(endWindow);
+            if (map.containsKey(right)) {
+                map.put(right, map.get(right) - 1);
+                if (map.get(right) == 0) {
+                    matched++;
+                }
+            }
+            while (matched == map.size()) {
+                if (minLen > endWindow - start + 1) {
+                    minLen = endWindow - start + 1;
+                    subStr = start;
+                }
+                char deleted = s.charAt(start++);
+                if (map.containsKey(deleted)) {
+                    if (map.get(deleted) == 0) {
+                        matched--;
+                    }
+                    map.put(deleted, map.get(deleted) + 1);
+                }
+            }
+        }
+        return minLen > s.length() ? "" : s.substring(subStr, subStr + minLen);
+    }
+}

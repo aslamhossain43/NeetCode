e609ef08555310623581174f9c4f4f8f80ca456a
Aslam <aslam@revesoft.com>
2024-09-29 10:31:48 +0600

    Min stack is added

---
+++ NeetCode/src/stack/MinStack.java
@@ -0,0 +1,38 @@
+package stack;
+
+import java.util.Stack;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Sep 29, 2024
+ */
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        // The min stack may be empty, so we need to check it
+        val = Math.min(val, minStack.isEmpty() ? val : minStack.peek());
+        minStack.push(val);
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}

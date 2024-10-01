66d3fcf26af54fe1f2248e6cde661b2cac279371
Aslam <aslam@revesoft.com>
2024-10-01 10:00:11 +0600

    2D matrix search is added

---
+++ NeetCode/src/binary_search/Matrix2DSearch.java
@@ -0,0 +1,28 @@
+package binary_search;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Oct 01, 2024
+ */
+public class Matrix2DSearch {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+        int target = 3;
+        System.out.println("Is target is in matrix: " + searchMatrix(matrix, target));
+    }
+
+    private static boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}

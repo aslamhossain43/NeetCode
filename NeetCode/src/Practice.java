52f7e64b1f40285818eb740109f5d026e4b44b58
Aslam <aslam@revesoft.com>
2024-10-01 09:17:25 +0600

    Binary search is added

---
+++ NeetCode/src/Practice.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;/**
+ *
+ * @Author Md. Aslam Hossain
+ * @Date Sep 30, 2024
+ *
+ */public class Practice {
+}
---
+++ NeetCode/src/binary_search/BinarySearch.java
@@ -0,0 +1,29 @@
+package binary_search;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Oct 01, 2024
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        System.out.println("Index is: " + search(nums, target));
+    }
+
+    private static int search(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return -1;
+    }
+}

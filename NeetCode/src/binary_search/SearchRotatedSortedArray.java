8c20405721e70e68af2c97220420c801d3736052
Aslam Hossain <aslam.ece.hstu@gmail.com>
2024-10-07 14:31:44 +0600

    Search rotated sorted array is added

---
+++ NeetCode/src/binary_search/SearchRotatedSortedArray.java
@@ -0,0 +1,38 @@
+package binary_search;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Oct 07, 2024
+ */
+public class SearchRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        System.out.println("Searched value is: " + search(nums, target));
+    }
+
+    private static int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[l] <= nums[mid]) {
+                if (target > nums[mid] || target < nums[l]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            } else {
+                if (target < nums[mid] || target > nums[r]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}

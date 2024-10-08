8773efb89d0841b622f19f66067bb166bd340dc9
Aslam <aslam@revesoft.com>
2024-10-08 12:04:02 +0600

    Reverse linked list is added

---
+++ NeetCode/src/linked_list/ReverseList.java
@@ -0,0 +1,49 @@
+package linked_list;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Oct 08, 2024
+ */
+public class ReverseList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" + "val=" + val + ", next=" + next + '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println("Reverse list is: " + reverseList(head));
+    }
+
+    private static ListNode reverseList(ListNode head) {
+        return rev(head, null);
+    }
+
+    private static ListNode rev(ListNode node, ListNode pre) {
+        if (node == null) return pre;
+        ListNode temp = node.next;
+        node.next = pre;
+        return rev(temp, node);
+    }
+}

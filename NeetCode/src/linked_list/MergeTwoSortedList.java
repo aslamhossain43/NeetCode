63a2f09ce7172aa9fddbcf9c119ff83b97aaea90
Aslam <aslam@revesoft.com>
2024-10-08 14:34:30 +0600

    Merged two sorted list is added

---
+++ NeetCode/src/linked_list/MergeTwoSortedList.java
@@ -0,0 +1,53 @@
+package linked_list;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Oct 08, 2024
+ */
+public class MergeTwoSortedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" + "val=" + val + ", next=" + next + '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+        System.out.println("Merged list is: " + mergeTwoLists(list1, list2));
+    }
+
+    private static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list2.next, list1);
+            return list2;
+        }
+    }
+}

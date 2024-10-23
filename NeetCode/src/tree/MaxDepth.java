7e409268335d7984c78b747da402fb8ac01481b6
Aslam <aslam@revesoft.com>
2024-10-23 09:35:16 +0600

    Max depth is added

---
+++ NeetCode/src/tree/MaxDepth.java
@@ -0,0 +1,53 @@
+package tree;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Oct 23, 2024
+ */
+public class MaxDepth {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println("Max depth is: " + maxDepth(root));
+
+    }
+
+    private static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
